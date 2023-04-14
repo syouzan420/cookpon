0b41f25a2929f2b2cf9bb4b362f645deaffbe76d
syouzan420 <syouzanponkikki@yahoo.co.jp>
2023-04-14 20:31:48 +0900

    update

--- src/MyApp.hs
+++ src/MyApp.hs
@@ -23,23 +23,22 @@ appMain :: IO ()
 appMain = do
   initializeAll
   F.initialize
-<<<<<<< HEAD
-  font1 <- F.load "font/monaco.ttf" fontSize 
-  font2 <- F.load "font/marugo.TTC" fontSize 
-  font3 <- F.load "font/oshide.otf" fontSize 
-  fontS1 <- F.blended font1 (V4 255 255 255 255) "abcdefghijklmnopqrstuvwxyz" 
-  fontS2 <- F.blended font2 (V4 255 255 255 255) hiragana
-  fontS3 <- F.blended font3 (V4 255 255 255 255) hiragana
-  mapM_ F.free [font1,font2,font3]
-  image <- load "images/takapon1.png"
-  image2 <- load "images/takapon2.png"
-=======
+-- <<<<<<< HEAD
+--  font1 <- F.load "font/monaco.ttf" fontSize 
+--  font2 <- F.load "font/marugo.TTC" fontSize 
+--  font3 <- F.load "font/oshide.otf" fontSize 
+--  fontS1 <- F.blended font1 (V4 255 255 255 255) "abcdefghijklmnopqrstuvwxyz" 
+--  fontS2 <- F.blended font2 (V4 255 255 255 255) hiragana
+--  fontS3 <- F.blended font3 (V4 255 255 255 255) hiragana
+--  mapM_ F.free [font1,font2,font3]
+--  image <- load "images/takapon1.png"
+--  image2 <- load "images/takapon2.png"
+-- =======
   I.initialize []
   fontS <- loadFonts fontSize ["font/monaco.ttf","font/marugo.TTC","font/oshide.otf"]
   (newState,fontTS) <- loadText fontSize "font/marugo.TTC" "texts/test.txt" initState 
   imageS <- loadImages ["images/cook5.png","images/cook6.png"]
   let myWindow = defaultWindow {windowInitialSize =  V2 480 600}
->>>>>>> 60ab7945918ba7dec5c5f028409f3612bc09332e
   window <- createWindow "COOKPON" myWindow
   renderer <- createRenderer window (-1) defaultRenderer
   ftexs <- mapM (createTextureFromSurface renderer) fontS 
@@ -51,13 +50,8 @@ appMain = do
   state <- newIORef initState 
   tm <- addTimer 30 (mainTimer state renderer ftexs itexs)
   ev <- addEventWatch (inputEvent state)
-<<<<<<< HEAD
-  M.withAudio M.defaultAudio 4096 $ do
-    M.load "music/test.mp3" >>= M.playMusic M.Forever
-=======
   M.withAudio M.defaultAudio 1024 $ do
-    M.load "music/cooktest2.mp3" >>= M.playMusic M.Forever
->>>>>>> 60ab7945918ba7dec5c5f028409f3612bc09332e
+    M.load "music/cooktest3.mp3" >>= M.playMusic M.Forever
     appLoop
   delEventWatch ev
   _ <- removeTimer tm
